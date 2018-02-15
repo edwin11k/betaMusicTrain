66f7cccc8903fb517517b226dd3ad7ee6e51699a
Edwin Lee <edwin11k@gmail.com>
2018-02-15 16:10:45 -0500

    Update music.py

--- music.py
+++ music.py
@@ -12,25 +12,30 @@ import os
 
 class Music(object): 
     """ Music object reading files
-    S : Speech file 
-    C : Classical Music file
-    R : Rock Music File
     
-    Within the given directory, the class will read all the files starts with the string & save
+    Within the given directory, the class will read all the files
     
-    Training files must be named to address the starting file
-    For example classical file in the directory must start with C 
     """
     
     
-    def __init__(self,dirPath=os.curdir,music_genre=None,window=0.05,step=0.02):
+    def __init__(self,dirPath=os.curdir,music_genre=None,window=0.05,step=0.05):
         self.genre=music_genre
         self.fileData=[]
         self.Fs=[]
         self.stFeatures=[]
         self.fileName=[]
+        self.fileInfo=[]
+        print()
+        
         for file in os.listdir(dirPath):
             [fs,x]=readAudioFile(dirPath+os.sep+file);x=stereo2mono(x)
+            
+            ## pydub seems to have already adjusted the frequency sampling for multi channel
+#            # If the signal is multichannel, reduce it down to mono for analysis
+#            channel=x.shape[1]
+#            if x.ndim>1:
+#                x=stereo2mono(x);fs=int(fs/channel)
+                    
             if music_genre=='Speech':
                 print(music_genre+" File Read Located At :"+dirPath+os.sep+file)
             else:
@@ -38,7 +43,23 @@ class Music(object):
             self.fileData.append(x);self.Fs.append(fs)
             self.stFeatures.append(stFeatureExtraction(x,fs,window*fs,step*fs)) 
             self.fileName.append(file)
+            # fileName, Sample Frame Rate, window duration, total duration,data Points from file
+            info=(file,fs,window,window*fs,x.shape[0]/fs,int(x.shape[0]/(step*fs)))
+            self.fileInfo.append(info)
+        print(self)
 
-    def __str__(self):    
-        return 'music'
+    def __str__(self):   
+        for i,mem in enumerate(self.fileInfo):
+            print()
+            print('File '+str(i+1)+':')
+            print('File Name:',mem[0])
+            print('File Sampling Rate:',mem[1])
+            print('Time Duration of each window:',mem[2],' sec')
+            print('Sample Number in each window:',mem[3])
+            print('Total Duration:',mem[4],'sec')
+            print('Data points from the file:',mem[5])
+        return 'Music Type:'+self.genre
+                            
+    
+    
     
